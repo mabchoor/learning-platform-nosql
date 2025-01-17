89bbad8674f172a3dabc66eeba553a71ed36263f
Abderrahmanedev23 <125931479+Abderrahmanedev23@users.noreply.github.com>
2025-01-17 22:38:12 +0100

    route

--- src/services/redisService.js
+++ src/services/redisService.js
@@ -3,11 +3,47 @@
 // Question: Quelles sont les bonnes pratiques pour les clés Redis ?
 // Réponse :
 
+const db = require("../config/db");
+
 // Fonctions utilitaires pour Redis
 async function cacheData(key, data, ttl) {
-    // TODO: Implémenter une fonction générique de cache
+  // TODO: Implémenter une fonction générique de cache
+  try {
+    const client = db.getRedisClient();
+    await client.set(key, JSON.stringify(data));
+    await client.expire(key, ttl);
+    console.log(`Data cached with key: ${key}`);
+  } catch (error) {
+    console.error("Error caching data:", error);
   }
-  
-  module.exports = {
-    // TODO: Exporter les fonctions utilitaires
-  };
+}
+
+async function getCachedData(key) {
+  try {
+    const client = db.getRedisClient();
+    const cachedData = await client.get(key);
+    if (!cachedData) {
+      return null;
+    }
+    console.log(`Data retrieved from cache with key: ${key}`);
+    return JSON.parse(cachedData);
+  } catch (error) {
+    console.error("Error getting cached data:", error);
+  }
+}
+async function deleteCachedData(key) {
+  try {
+    const client = db.getRedisClient();
+    await client.del(key);
+    console.log(`Data deleted from cache with key: ${key}`);
+  } catch (error) {
+    console.error("Error deleting cached data:", error);
+  }
+}
+
+module.exports = {
+  // TODO: Exporter les fonctions utilitaires
+  cacheData,
+  getCachedData,
+  deleteCachedData,
+};

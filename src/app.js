f2845fbf7dd42b64da8ca27304604dab423c0510
mabchoor <147151452+mabchoor@users.noreply.github.com>
2025-01-17 23:31:39 +0100

    final

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 // Question: Comment organiser le point d'entrée de l'application ?
 // Question: Quelle est la meilleure façon de gérer le démarrage de l'application ?
 
-const express = require('express');
-const config = require('./config/env');
-const db = require('./config/db');
+const express = require("express");
+const config = require("./config/env");
+const db = require("./config/db");
 
-const courseRoutes = require('./routes/courseRoutes');
-const studentRoutes = require('./routes/studentRoutes');
+const courseRoutes = require("./routes/courseRoutes");
+const studentRoutes = require("./routes/studentRoutes");
 
 const app = express();
 
@@ -16,15 +16,31 @@ async function startServer() {
     // TODO: Configurer les middlewares Express
     // TODO: Monter les routes
     // TODO: Démarrer le serveur
+
+    await db.connectMongo();
+    await db.connectRedis();
+
+    app.use(express.json());
+
+    app.use("/courses", courseRoutes);
+    app.use("/students", studentRoutes);
+
+    app.listen(config.port, () => {
+      console.log(`Server is running on http://localhost:${config.port}`);
+    });
   } catch (error) {
-    console.error('Failed to start server:', error);
+    console.error("Failed to start server:", error);
     process.exit(1);
   }
 }
 
 // Gestion propre de l'arrêt
-process.on('SIGTERM', async () => {
+process.on("SIGTERM", async () => {
   // TODO: Implémenter la fermeture propre des connexions
+  await db.closeMongo();
+  await db.closeRedis();
+  console.log("Server is shutting down");
+  process.exit(0);
 });
 
 startServer();
